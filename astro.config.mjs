e6e736a7d1e75a5c1b3b0878cf4ae2e31961b395
fm-anderson <78233612+fm-anderson@users.noreply.github.com>
2024-06-07 21:38:32 -0400

    feat: netlify ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
+import netlify from "@astrojs/netlify";
 import db from "@astrojs/db";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon(), db()]
+  integrations: [react(), tailwind(), icon(), db()],
+  output: "server",
+  adapter: netlify(),
 });
